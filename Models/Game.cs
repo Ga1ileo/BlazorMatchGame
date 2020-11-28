4723ce1a9f42fa451bef35aa0ff63137ed47c021
Maria Isabel Alberto Del Orbe <maria_alberto1@ucne.edu.do>
2020-11-28 12:18:37 -0400

    Entidad Game

---
+++ Models/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMatchGame.Models
+{
+    public class Game
+    {
+        public string Nombre { get; set; }
+        public string Mensaje { get; set; }
+        public string Tiempo { get; set; }
+    }
+}
